6bcf386ae25374627fbf9f0cc1e7fa72a527540d
danj-O <danjowanjo@gmail.com>
2020-02-28 17:56:08 -0500

    testing margins on phone

--- src/templates/items.js
+++ src/templates/items.js
@@ -28,31 +28,13 @@ export const query = graphql`
 
 
 //this is the template for all blog posts
-export default function Projects(props) {
-  // const options = {
-  //   renderNode: {
-  //     //override specific node types
-  //     "embedded-asset-block": (node)=>{
-  //       //PROBLEM!!!!!!------------
-  //       //Where should the query go to find these two in playground??!!
-  //       const alt = node.data.target.fields.title["en-US"]
-  //       const url = node.data.target.fields.file["en-US"].url
-  //       console.log(alt, url)
-  //       return <img alt={alt} src={url}/>
-  //     }
-  //   }
-  // }
+export default function Items(props) {
+
   return (
     <Layout>
       <Head title={props.data.contentfulItem.title} />
       <h1>{ props.data.contentfulItem.title }</h1>
       {/* <p>{ props.data.contentfulProjects.datePublished }</p> */}
-      {/* <a href={props.data.contentfulProjects.githubUrl} target= "_blank" rel="noopener noreferrer">
-        Github Source
-      </a> */}
-      {/* <a href={props.data.contentfulProjects.liveUrl} target= "_blank" rel="noopener noreferrer">
-        Live
-      </a> */}
       <img src={props.data.contentfulItem.itemImage.file.url} alt={props.data.contentfulItem.itemImage.fileName}/>
       {/* <img src={props.data.contentfulAsset.file.url} alt=""/> */}
       {documentToReactComponents(props.data.contentfulItem.description.json)}
