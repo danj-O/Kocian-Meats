1e5362492335ddf016556d48a122b3a2792881b8
danj-O <danjowanjo@gmail.com>
2020-03-15 16:49:03 -0400

    deleted ssr file

--- gatsby-srr.js
+++
@@ -1,9 +0,0 @@
-// import React from 'react'
-// import Layout from './src/layouts'
-
-// const transitionDelay = 800;
-
-
-// export const wrapPageElement = ({ element, props }) => {
-//   return <Layout {...props}>{element}</Layout>;
-// };
