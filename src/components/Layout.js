78d47d64906fb0dc8db384de0df593bcb7cb8994
danj-O <danjowanjo@gmail.com>
2020-03-18 13:45:27 -0400

    also removed footer to test warnings

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Footer from './Footer'
+// import Footer from './Footer'
 import layoutStyles from './layout.module.scss'
 // import Navigation from './Navigation'
 
@@ -12,7 +12,7 @@ export default function Layout({children, location}) {
           <div className={layoutStyles.content}> 
                 {children}
           </div>
-        <Footer />
+        {/* <Footer /> */}
       </div>
   )
 }
